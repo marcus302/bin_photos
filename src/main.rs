c0693986dcd425b5abd9f640f5364f68cbf8085d
Mark de Wijk <dewijkmark@gmail.com>
2021-06-18 14:31:32 +0200

    Trying to fill a vector with user input. Not working so far.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let num_classes = 5;
+    let mut classes = vec![""; num_classes];
+    let mut class = String::new();
+    for iter in 0..num_classes { 
+        io::stdin()
+            .read_line(&mut class)
+            .expect("Failed to read line.");
+        classes[iter] = class.trim();
+        let mut class = String::new();
+    }
+    println!("{:?}", classes);
 }
